7c8eee60e46a92890f9caf97aa1cf849de1608fd
brunodovalcallao <24bdovalcallao@athenian.org>
2021-11-04 22:35:07 -0700

    hey I didn't have enough time to finish this, but I think i'm close, can we go over this during class tomorrow? Thanks.

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -5,19 +5,36 @@ public class GroceryList {
     // Remember to create the array!
     // You don't need to change any of the values in the array
     public GroceryList() {
-        // YOUR CODE HERE
+        arr = new String[10];
     }
 
     // Adds an item to the grocery list
     public void add(String item) {
-        // YOUR CODE HERE
+        for( int index =arr.length-2; index >= 0 ; index-- ) {
+            arr[index + 1] = arr[index];
+        }
+        arr[0] = item;
     }
 
     // Removes an item from the grocery list
     // Replaces the item with null
     // Remember to shift anything to the left if necessary
     public void remove(String item) {
-        // YOUR CODE HERE
+        int index = 0;
+
+        for(int x = 0; x<arr.length;x++){
+            if(arr[x] != null && arr[x].equals(item)){
+                index = x;
+            }
+        }
+        arr[index] = null;
+        for(int i = index + 1; i < arr.length; i++) {
+            if (arr[i] != null) {
+                String temp = arr[1];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
     }
 
     // Returns a String representation the grocery list
@@ -29,7 +46,13 @@ public class GroceryList {
     // You **may** have an extra comma at the end
     @Override
     public String toString() {
-        // YOUR CODE HERE
-        return "";
+        String message = "Grocery List: ";
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i] != null){
+                message+= arr[i];
+                message+=", ";
+            }
+        }
+        return message;
     }
 }
